acef55ad8259258617e5c0fe415f18b085a41d83
Adriano Regis <adbrum@outlook.com>
2021-08-19 11:30:54 +0100

    Added login page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# swi-front-end
+# SWI - Simple Web Interface
+
+## 1 - register Requirements
+
+## 2 - login Test Driven
+
+## 3 - upload an image
+
+## 4 - list my uploaded images with thumbnails
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/globals';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
